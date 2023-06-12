f5ead7a5a14070594832838ff6b55c7241cbf312
Santiago Valero <santiagovalero99@gmail.com>
2023-06-12 18:58:34 -0300

    navbar nombres

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ export const Navbar = () => {
     <header>
         <img src="" alt="" />
         <nav>
-            <p><Link to="Informacion" spy={true} smooth={true} offstet={50} duration={500} ><button>ABOUT US</button></Link></p>
-            <p><Link to="Servicios" spy={true} smooth={true} offstet={50} duration={500} ><button>SERVICES</button></Link></p>
-            <p><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button>CLIENTS</button></Link></p>
-            <p><Link to="Contacto" spy={true} smooth={true} offstet={50} duration={500} ><button>CONTACT</button></Link></p>
+            <p><Link to="Informacion" spy={true} smooth={true} offstet={50} duration={500} ><button>INFORMACION</button></Link></p>
+            <p><Link to="Servicios" spy={true} smooth={true} offstet={50} duration={500} ><button>SERVICIOS</button></Link></p>
+            <p><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button>CARACTERISTICAS</button></Link></p>
+            <p><Link to="Contacto" spy={true} smooth={true} offstet={50} duration={500} ><button>CONTACTO</button></Link></p>
         </nav>
       </header>
   )
