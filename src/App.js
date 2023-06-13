cbe22b114d7aec5cf8a176e2ec86bd4783b8b37b
Santiago Valero <santiagovalero99@gmail.com>
2023-06-13 19:25:18 -0300

    comillas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar.jsx";
 import SPA from "./Components/SPA/SPA.jsx";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Footer from './Components/Footer/Footer.jsx'
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer/Footer.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />;
-        <Routes>
-          <Route path='/' element={<SPA/>} />
-        </Routes>
-      <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<SPA />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-     
-    
-  )
-  
+  );
 }
 
 export default App;
