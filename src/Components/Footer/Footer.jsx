a6a31912dd0383ab56529d23cb04e389d2b86cf2
Matias Roccazzella <matias.a.re@hotmail.com>
2023-06-13 18:32:03 -0300

    cambio

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footerContainer'>
+        <div>Derechos reservados / SataCore </div>
+    </div>
+  )
+}
+
+export default Footer
