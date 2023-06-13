8a0e9c89a053ab5e4c59ab86b16c8d5839cd4e2d
Matias Roccazzella <matias.a.re@hotmail.com>
2023-06-13 18:14:07 -0300

    cambio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar.jsx";
 import SPA from "./Components/SPA/SPA.jsx";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Footer from './Components/Footer/Footer.jsx'
+
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />;
-      <SPA/>
-    </>
+        <Routes>
+          <Route path='/' element={<SPA/>} />
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
+     
+    
   )
   
 }
