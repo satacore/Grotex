cc34c938c77a74c0e73be495bb3fb8253c3cc920
Matias Roccazzella <matias.a.re@hotmail.com>
2023-06-13 19:21:24 -0300

    base

---
+++ src/Components/Caracteristicas/Caracteristicas.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Caracteristicas.css'
+
+const Caracteristicas = () => {
+  return (
+    <div className='CaracteristicasContainer'>
+        <div id='Caracteristicas' className='generalTextContainer'>
+            <p className='caracteristicasP1'> <span className='p1Left'>'03' | </span> CARACTERÍSTICAS</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default Caracteristicas
--- src/Components/SPA/SPA.jsx
+++ src/Components/SPA/SPA.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Servicios from '../Servicios/Servicios.jsx'
 import Informacion from '../Informacion/Informacion.jsx'
+import Caracteristicas from '../Caracteristicas/Caracteristicas.jsx'
 
 const SPA = () => {
   return (
     <>
     <Informacion />
     <Servicios />
+    <Caracteristicas/>
     </>
   )
 }
