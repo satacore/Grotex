3d3fe49997662e523649389a644f6a77657d7eab
Matias Roccazzella <matias.a.re@hotmail.com>
2023-06-28 18:26:26 -0300

    contacto

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -4,9 +4,27 @@ import './Contacto.css'
  const Contacto = () => {
   return (
     <div className='contactoContainer'>
-        <div className='formulario'>
-            
-        </div>
+      <div className='contactoTitulo'>
+        <h2 className='titleForm'>QUIRE TRABAJAR CON NOSOTROS? <br /> <br /><span className='spanForm'>CONTÁCTENOS</span></h2>
+      </div>
+      <br />
+      <div className='formContainer'>
+            <form className='formulario' action="">
+              <label htmlFor="">
+                Name: 
+                <br />
+                <input type="text" name='nombre' placeholder='Ingrese su nombre y apellido' size={40}/>
+                <br />
+                Correo electronico: 
+                <br />
+                <input type="email" name='email' placeholder='Ingrese su correo electronico' size={40}/>
+                <br />              
+                Escriba su consulta: 
+                <br />
+                <input type="text" name='descripion' placeholder='Escriba su consulta aquí' size={100} />
+              </label>
+            </form>
+      </div>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-scroll'
-import {NavLink} from "react-router-dom";
+import {Link as Link2} from "react-router-dom";
 import './Navbar.css'
 import Logo1 from '../../assets/Logo1.png'
 
@@ -8,13 +8,13 @@ import Logo1 from '../../assets/Logo1.png'
 export const Navbar = () => {
   return (
     <header>
-        <NavLink to="/"><img className='LogoImg' src={Logo1} alt="" /></NavLink>
+        <Link2 to="/"><img className='LogoImg' src={Logo1} alt="" /></Link2>
         <nav className='NavBarContainer'>
           <div className='NavBarItems'>
             <p><Link to="Informacion" spy={true} smooth={true} offstet={50} duration={500} ><button>INFORMACION</button></Link></p>
             <p><Link to="Servicios" spy={true} smooth={true} offstet={50} duration={500} ><button>SERVICIOS</button></Link></p>
             <p><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button>CARACTERISTICAS</button></Link></p>
-            <p><NavLink to="/Contacto" ><button>CONTACTO</button></NavLink></p>
+            <p><Link2 to="/Contacto" ><button>CONTACTO</button></Link2></p>
           </div> 
         </nav>
     </header>
