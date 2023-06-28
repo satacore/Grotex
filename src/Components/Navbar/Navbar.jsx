4ef5c635bb1c95071546d6306ac8a71d05abe6d0
Santiago Valero <santiagovalero99@gmail.com>
2023-06-28 18:26:43 -0300

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
         <nav>
             <p><Link to="Informacion" spy={true} smooth={true} offstet={50} duration={500} ><button>INFORMACION</button></Link></p>
             <p><Link to="Servicios" spy={true} smooth={true} offstet={50} duration={500} ><button>SERVICIOS</button></Link></p>
-            <p><Link to="clients" spy={true} smooth={true} offstet={50} duration={500} ><button>CARACTERISTICAS</button></Link></p>
+            <p><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button>CARACTERISTICAS</button></Link></p>
             <p><NavLink to="/Contacto" spy={true} smooth={true} offstet={50} duration={500} ><button>CONTACTO</button></NavLink></p>
         </nav>
     </header>
