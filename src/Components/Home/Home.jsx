220ae4635034e7e588dcfefb40bc4212bf0abfc6
Santiago Valero <santiagovalero99@gmail.com>
2023-06-28 19:05:25 -0300

    home

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css';
+import React from 'react';
+
+
+const Home = () => {
+
+  return (
+  <div className='generalContainer' id='top'>
+    <div className='sloganContainer'>
+      <h1 className='slogan'>AGENCY</h1>
+      <h1 className='slogaSubtitle'>ECOMING</h1>
+    </div>
+  </div>
+  )
+}
+
+export default Home
--- src/Components/SPA/SPA.jsx
+++ src/Components/SPA/SPA.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Servicios from '../Servicios/Servicios.jsx'
 import Informacion from '../Informacion/Informacion.jsx'
 import Caracteristicas from '../Caracteristicas/Caracteristicas.jsx'
+import Home from '../Home/Home.jsx'
 
 const SPA = () => {
   return (
     <>
+    <Home />
     <Informacion />
     <Servicios />
     <Caracteristicas/>
