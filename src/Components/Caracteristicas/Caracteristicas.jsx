ef73d36e23db436f56af5a66a380745aeb8afef2
Santiago Valero <santiagovalero99@gmail.com>
2023-07-06 13:10:43 -0300

    agregado de textos de las secciones servicios y caracteristicas

--- src/Components/Caracteristicas/Caracteristicas.jsx
+++ src/Components/Caracteristicas/Caracteristicas.jsx
@@ -7,6 +7,18 @@ const Caracteristicas = () => {
         <div id='Caracteristicas' className='generalTextContainer'>
           <div className='titleContainer'>
             <p className='p1'> <span className='p1Left'>*03* | </span> CARACTERÍSTICAS</p>
+            <h1 className='s1'>SOCIAL MEDIA *</h1>
+            <p className='t1'>Estrategia integral para redes: Instagram, Facebook, Tik Tok, Linkedin, Youtube.</p>
+            <h1 className='s1'>ADWORDS *</h1>
+            <p className='t1'>Pauta en Google. Obtención de ventas y leads. Search, Google shopping, Display y Remarketing.</p>
+            <h1 className='s1'>SEO *</h1>
+            <p className='t1'>Fomentar y mejorar el posicionamiento orgánico de tu web. Blog, alt, kw, términos de búsqueda.</p>
+            <h1 className='s1'>EMAIL MKT *</h1>
+            <p className='t1'>Campañas inteligentes de Email mkt para crear vínculos sólidos con los consumidores.</p>
+            <h1 className='s1'>MERCADO LIBRE *</h1>
+            <p className='t1'>Somos Mercado Libre Partners. Desarrollamos estrategias integrales para crecer en ventas.</p>
+            <h1 className='s1'>ECOMMERCE *</h1>
+            <p className='t1'>Diseño, maquetado y programación de ecommerce. Desarrollos en diferentes plataformas.</p>
           </div>
         </div>
     </div>
--- src/Components/Servicios/Servicios.jsx
+++ src/Components/Servicios/Servicios.jsx
@@ -7,6 +7,14 @@ const Servicios = () => {
         <div id='Servicios' className='generalTextContainer'>
           <div className='titleContainer'>
             <p className='p1'> <span className='p1Left'>*02* | </span> SERVICIOS</p>
+            <h1 className='s1'>INVESTIGACIÓN Y ANÁLISIS *</h1>
+            <p className='t1'>Primer instancia de relevamiento completo de la empresa / negocio y su competencia. Entender donde estamos parados.</p>
+            <h1 className='s1'>ESTRATEGIA *</h1>
+            <p className='t1'>Armado del plan general. Establecer expectativas y alcance de cada una de las herramientas. Calendarización de las etapas.</p>
+            <h1 className='s1'>EJECUCIÓN *</h1>
+            <p className='t1'>Contamos con un equipo de profesionales que permiten llevar a cabo cada uno de los objetivos planteados.</p>
+            <h1 className='s1'>PASOS A SEGUIR *</h1>
+            <p className='t1'>La era online permite un seguimiento real de cada una de las herramientas que se ponen en práctica.</p>
           </div>
         </div>
     </div>
