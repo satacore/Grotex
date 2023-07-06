5e9ec00997e74dc65526a224189528421462d4b1
Santiago Valero <santiagovalero99@gmail.com>
2023-07-06 08:27:35 -0300

    footer changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './Footer.css'
-import SataCore from '../../assets/SataCore.png'
 
 const Footer = () => {
   return (
     <div className='footerContainer'>
-        <p className='FooterText'>© 2023 Ecoming | Todos los derechos reservados| Contacto | Diseñada por: <img className='SataCoreFooter' src={SataCore} alt="SataCore"/></p>
+        <p className='FooterText'>© 2023 Ecoming | Todos los derechos reservados | Diseñada por: Satacore</p>
     </div>
   )
 }
