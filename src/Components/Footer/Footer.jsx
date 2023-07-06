b147c8a857f804fd71345865e0ef6a79dcb4050f
Santiago Valero <santiagovalero99@gmail.com>
2023-07-06 12:48:26 -0300

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,8 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footerContainer'>
-        <p className='FooterText'>© 2023 Ecoming | Todos los derechos reservados | Diseñada por: Satacore</p>
+        <p className='FooterText1'>INFORMACION | SERVICIOS | CARACTERISTICAS | CONTACTO</p>
+        <p className='FooterText'>© 2023 Ecoming | All rights reserved | Privacy | Terms of Use | Desinged By: Satacore</p>
     </div>
   )
 }
