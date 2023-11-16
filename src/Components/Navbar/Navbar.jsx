71cb78253f087f6e6db6582a9aec5e60011895c3
Matias Roccazzella <matias.a.re@hotmail.com>
2023-11-16 18:22:06 -0300

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,10 +14,10 @@ const Navbar = () => {
     <header className='headerNav'>
         <NavLink to="/"><img  className='LogoImg' src={Logo3} alt="" /></NavLink>
         <nav className='navBarMain'>
-            <p id='infoButton' className='navLinks'><Link to="Informacion" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>INFORMACION *</button></Link></p>
-            <p id='serviciosButton' className='navLinks'><Link to="Servicios" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>SERVICIOS *</button></Link></p>
-            <p id='caracteristicasButton' className='navLinks'><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>CARACTERISTICAS *</button></Link></p>
-            <p id='contactoButton' className='navLinks'><NavLink to="/Contacto" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>CONTACTO *</button></NavLink></p>
+            <p id='infoButton' className='navLinks'><Link to="Informacion" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>ABOUT</button></Link></p>
+            <p id='serviciosButton' className='navLinks'><Link to="Servicios" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>SERVICIES</button></Link></p>
+            <p id='caracteristicasButton' className='navLinks'><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>LOGIN</button></Link></p>
+            <p id='contactoButton' className='navLinks'><NavLink to="/Contacto" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>CONTACT</button></NavLink></p>
         </nav>
     </header>
   )
