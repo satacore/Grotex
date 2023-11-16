5cf53ed89fa99ebeb17d69399fd9e3b113ad8e15
Santiago Valero <santiagovalero99@gmail.com>
2023-11-16 18:48:23 -0300

    second push

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import './Home.css';
 import React from 'react';
 import HomeLogo2 from '../../assets/HomeLogo2.png'
-import Partners from '../../assets/Partners.png'
 
 
 
@@ -11,9 +10,8 @@ const Home = () => {
   <>
   <div className='generalContainer' id='top'>
     <div className='sloganContainer'>
-      <h1 className='slogaSubtitle' style={{cursor: "default"}}>ECOMING *</h1>
-      <h1 className='slogan' style={{cursor: "default"}}>AGENCY</h1>
-      <img className='partnerImg' src={Partners} alt="" />
+      <h1 className='slogaSubtitle' style={{cursor: "default"}}>GROTEX</h1>
+      <h1 className='slogan' style={{cursor: "default"}}>ADS</h1>
       <img className='homeImg' src={HomeLogo2} alt="" />
     </div>
   </div>
