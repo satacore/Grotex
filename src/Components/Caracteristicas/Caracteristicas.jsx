4bf6eec9537dfffc24281f26bd7bcc8281d39b7a
Santiago Valero <santiagovalero99@gmail.com>
2023-11-17 17:54:13 -0300

    askdk

--- src/Components/Caracteristicas/Caracteristicas.jsx
+++ src/Components/Caracteristicas/Caracteristicas.jsx
@@ -9,18 +9,30 @@ const Caracteristicas = () => {
             <p className='p1'>CARACTERÍSTICAS</p>
               <div className='cardContainer'>
                 <div className='boxContainer'> 
+                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 21">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 3.464V1.1m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175C17 15.4 17 16 16.462 16H3.538C3 16 3 15.4 3 14.807c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 10 3.464ZM1.866 8.832a8.458 8.458 0 0 1 2.252-5.714m14.016 5.714a8.458 8.458 0 0 0-2.252-5.714M6.54 16a3.48 3.48 0 0 0 6.92 0H6.54Z"/>
+  </svg>
                   <h1 className='s1'>PUSH NOTIFICATIONS</h1>
                   <p className='t1'>Earn money from your website, blog or other traffic source. Our push technology does not replace any of your current ad units. It's almost like you are earning daily income in the background with out anyone even knowing.</p>
                 </div>
                 <div className='boxContainer'> 
+                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 3a3 3 0 1 1-1.614 5.53M15 12a4 4 0 0 1 4 4v1h-3.348M10 4.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0ZM5 11h3a4 4 0 0 1 4 4v2H1v-2a4 4 0 0 1 4-4Z"/>
+  </svg>
                   <h1 className='s1'>LEAD GENERATION</h1>
                   <p className='t1'>We deliver successful campaigns across multiple business sectors including surveys, market research, debt, payday, insurance, business opportunity, education, credit repair, health & beauty and more.</p>
                 </div>
                 <div className='boxContainer'> 
+                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4a4 4 0 0 1 4 4v6M5 4a4 4 0 0 0-4 4v6h8M5 4h9M9 14h10V8a3.999 3.999 0 0 0-2.066-3.5M9 14v5m0-5h4v5m-9-8h2m8-4V1h2"/>
+  </svg>
                   <h1 className='s1'>EMAIL MARKETING</h1>
                   <p className='t1'>Communicate one on one with potential consumers using this highly scalable and cost-effective solution. With data driven targeting and analytics, enjoy leading deliverability and high open email and response rates.</p>
                 </div>
-                <div className='boxContainer'> 
+                 <div className='boxContainer'> 
+                  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16.344 12.168-1.4-1.4a1.98 1.98 0 0 0-2.8 0l-.7.7a1.98 1.98 0 0 1-2.8 0l-2.1-2.1a1.98 1.98 0 0 1 0-2.8l.7-.7a1.981 1.981 0 0 0 0-2.8l-1.4-1.4a1.828 1.828 0 0 0-2.8 0C-.638 5.323 1.1 9.542 4.78 13.22c3.68 3.678 7.9 5.418 11.564 1.752a1.828 1.828 0 0 0 0-2.804Z"/>
+  </svg>
                   <h1 className='s1'>SURVEY</h1>
                   <p className='t1'>Top EPC Nutra & eCommerce Survey for Email and SMS.</p>
                 </div>
