91aabb800874ba2f7950870db5400e607ff85487
Santiago Valero <santiagovalero99@gmail.com>
2023-11-17 17:01:39 -0300

    general changes

--- src/Components/Servicios/Servicios.jsx
+++ src/Components/Servicios/Servicios.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import './servicios.css'
-import Ejec from '../../assets/Ejecucion.png'
-import Estr from '../../assets/Estrategia.png'
-import Paso from '../../assets/Pasos.png'
-
 const Servicios = () => {
   return (
     <div className='ServiciosContainer'>
@@ -13,17 +9,23 @@ const Servicios = () => {
             <div className='cardContainer'>
               
               <div className='boxContainer'>
-                <img className='ServiciosImg' src={Estr} alt="" />
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1v14h16M4 10l3-4 4 4 5-5m0 0h-3.207M16 5v3.207"/>
+  </svg>
                 <h1 className='s1'>FROM ZERO TO ONE</h1>
                 <p className='t1'>We are moving from zero to one, not from one to hundred. That means we are focused on creating new solutions, not copying the existing ones.</p>
               </div>
               <div className='boxContainer'>
-                <img className='ServiciosImg' src={Ejec} alt="" />
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 20">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16.5A2.493 2.493 0 0 1 6.51 18H6.5a2.468 2.468 0 0 1-2.4-3.154 2.98 2.98 0 0 1-.85-5.274 2.468 2.468 0 0 1 .921-3.182 2.477 2.477 0 0 1 1.875-3.344 2.5 2.5 0 0 1 3.41-1.856A2.5 2.5 0 0 1 11 3.5m0 13v-13m0 13a2.492 2.492 0 0 0 4.49 1.5h.01a2.467 2.467 0 0 0 2.403-3.154 2.98 2.98 0 0 0 .847-5.274 2.468 2.468 0 0 0-.921-3.182 2.479 2.479 0 0 0-1.875-3.344A2.5 2.5 0 0 0 13.5 1 2.5 2.5 0 0 0 11 3.5m-8 5a2.5 2.5 0 0 1 3.48-2.3m-.28 8.551a3 3 0 0 1-2.953-5.185M19 8.5a2.5 2.5 0 0 0-3.481-2.3m.28 8.551a3 3 0 0 0 2.954-5.185"/>
+  </svg>
                 <h1 className='s1'>OUR LEVEL UP</h1>
                 <p className='t1'>With our dedicated team of professionals, NeptuneAds helps you through the entire process from creative design, placement, tracking and optimization.</p>
               </div>
               <div className='boxContainer'>
-                <img className='ServiciosImg' src={Paso} alt="" />
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+    <path d="M17.947 2.053a5.209 5.209 0 0 0-3.793-1.53A6.414 6.414 0 0 0 10 2.311 6.482 6.482 0 0 0 5.824.5a5.2 5.2 0 0 0-3.8 1.521c-1.915 1.916-2.315 5.392.625 8.333l7 7a.5.5 0 0 0 .708 0l7-7a6.6 6.6 0 0 0 2.123-4.508 5.179 5.179 0 0 0-1.533-3.793Z"/>
+  </svg>
                 <h1 className='s1'>YOUR SATISFACTION</h1>
                 <p className='t1'>Working with us you will work with professional business developers and media buyers having the vast good experience.</p>
               </div>
