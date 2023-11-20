5f725fe2215bcdd5a480f707cd5d9d89fadce742
Santiago Valero <santiagovalero99@gmail.com>
2023-11-20 14:39:07 -0300

    agregado boton nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,7 +16,8 @@ const Navbar = () => {
         <nav className='navBarMain'>
             <p id='infoButton' className='navLinks'><Link to="Informacion" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>ABOUT</button></Link></p>
             <p id='serviciosButton' className='navLinks'><Link to="Servicios" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>SERVICIES</button></Link></p>
-            <p id='caracteristicasButton' className='navLinks'><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>LOGIN</button></Link></p>
+            <p id='caracteristicasButton' className='navLinks'><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>FEATURES</button></Link></p>
+            <p id='caracteristicasButton' className='navLinks'><Link to="Caracteristicas" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>LOG IN</button></Link></p>
             <p id='contactoButton' className='navLinks'><NavLink to="/Contacto" spy={true} smooth={true} offstet={50} duration={500} ><button className='navButton'>CONTACT</button></NavLink></p>
         </nav>
     </header>
